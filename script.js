94e54ec05867fdb22bdcbc393e8c42e8d7132ec4
ArelyAyalaR <arelyayalareyes@gmail.com>
2024-08-16 18:47:07 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// alert("Hola Mundo <3");
+
+
+let app = document.getElementById('app');
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+// reconoces una accion porque tiene paréntesis después del nombre de la accion
+// a veces tiene el operador punto
+typewriter
+  .pauseFor(2500) // milisegundos 2.5 segundos
+  .typeString('Arely Ayala')
+  .pauseFor(300)
+  .deleteAll()
+  .typeString('Desarrolladora FRONTEND JR')
+  .pauseFor(1000)
+  .start();
+
+let frase = document.getElementById('frase');
+let typewriterFrase = new Typewriter(frase, {
+  loop: true,
+  delay: 75,
+});
+// reconoces una accion porque tiene paréntesis después del nombre de la accion
+// a veces tiene el operador punto
+typewriterFrase
+  .pauseFor(2500) // milisegundos 2.5 segundos
+  .typeString('"...Por lo tanto, lo que debemos hacer es esforzarnos y perseverar y NUNCA RENDIRNOS."')
+  .pauseFor(300)
+  .deleteAll()
+  .typeString('Dalai Lama IX')
+  .pauseFor(1000)
+  .start();
+
+
+
+async function copiarAlPortapapeles(texto) {
+  try {
+    await navigator.clipboard.writeText(texto);
+    alert('Texto copiado al portapapeles');
+  } catch (err) {
+    console.error('Error al copiar:', err);
+  }
+}
